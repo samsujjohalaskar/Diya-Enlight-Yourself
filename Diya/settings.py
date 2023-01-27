293e5d16602c975610cc8e9b27d3011a1413dfb5
SAMSUJJOHA LASKAR <samsujjohalaskar@gmail.com>
2023-01-27 20:09:28 +0530

    fixingCarousel

--- Diya/settings.py
+++ Diya/settings.py
@@ -16,6 +16,11 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+STATICFILES_DIRS = [
+    "Diya/static",
+]
+
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
